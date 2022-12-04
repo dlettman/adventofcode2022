f673c14bc744343f77fc150eba85e78ab910c966
dlettman <derek.lettman@gmail.com>
2022-12-03 22:06:56 -0800

    solve day 4

--- 04/exercise_04.py
+++ 04/exercise_04.py
@@ -2,21 +2,31 @@ import sys
 sys.path.append("..")
 import helpers
 
+def get_mins_and_maxes(line):
+    elf1, elf2 = line.split(",")
+    elf1_min, elf1_max = [int(item) for item in elf1.split("-")]
+    elf2_min, elf2_max = [int(item) for item in elf2.split("-")]
+    return (elf1_min, elf1_max, elf2_min, elf2_max)
+
 def part_one(input_filename):
     input = helpers.parse_input(input_filename)
-    if not input:
-        return "*** NO INPUT SUPPLIED ***"
-    # do stuff here
-    output = input
-    return output
+    containers = 0
+    for line in input:
+        elf1_min, elf1_max, elf2_min, elf2_max = get_mins_and_maxes(line)
+        if (elf1_min <= elf2_min and elf1_max >= elf2_max) or (elf1_min >= elf2_min and elf1_max <= elf2_max):
+            containers += 1
+    return containers
 
 def part_two(input_filename):
     input = helpers.parse_input(input_filename)
-    if not input:
-        return "*** NO INPUT SUPPLIED ***"
-    # do stuff here
-    output = input
-    return output
+    overlappers = 0
+    for line in input:
+        elf1_min, elf1_max, elf2_min, elf2_max = get_mins_and_maxes(line)
+        e1 = set(range(elf1_min, elf1_max + 1))
+        e2 = set(range(elf2_min, elf2_max + 1))
+        if e1.intersection(e2):
+            overlappers += 1
+    return overlappers
 
 if __name__ == "__main__":
     print("*** PART ONE ***\n")
