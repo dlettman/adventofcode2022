9d0a27ba481cceac78d8bf7938f2b2c5a17cba57
dlettman <derek.lettman@gmail.com>
2022-12-05 21:12:41 -0800

    solve day 6

--- 06/exercise_06.py
+++ 06/exercise_06.py
@@ -1,22 +1,25 @@
 from helpers import helpers
+from collections import deque
 
+
+def get_unique_char_marker(input_string, char_count):
+    chars = deque()
+    for idx, char in enumerate(input_string):
+        chars.append(char)
+        if len(set(chars)) == char_count:
+            return idx
+        if len(chars) >= char_count:
+            chars.popleft()
+    return None
 
 def part_one(input_filename):
-    input = helpers.parse_input(input_filename)
-    if not input:
-        return "*** NO INPUT SUPPLIED ***"
-    # do stuff here
-    output = input
-    return output
+    input_text = helpers.parse_input(input_filename)
+    return get_unique_char_marker(input_text[0], 4)
 
 
 def part_two(input_filename):
-    input = helpers.parse_input(input_filename)
-    if not input:
-        return "*** NO INPUT SUPPLIED ***"
-    # do stuff here
-    output = input
-    return output
+    input_text = helpers.parse_input(input_filename)
+    return get_unique_char_marker(input_text[0], 14)
 
 
 if __name__ == "__main__":
