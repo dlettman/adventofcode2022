445fa731dcffadb100657d6f01a00474837ac53f
dlettman <derek.lettman@gmail.com>
2022-12-09 21:44:49 -0800

    solve day 10

--- 09/exercise_09.py
+++ 09/exercise_09.py
@@ -40,16 +40,22 @@ def plot_ropes(ropes, trail):
     max_y = max([item[1] for item in ropes] + [item[1] for item in places])
     min_x = min(0, min([item[0] for item in ropes] + [item[0] for item in places]))
     min_y = min(0, min([item[1] for item in ropes] + [item[1] for item in places]))
-    rope_map = [[" ." for _ in range((max_x - min_x)+ 1)] for _ in range((max_y - min_y) + 1)]
+    rope_map = [["🟫" for _ in range((max_x - min_x)+ 1)] for _ in range((max_y - min_y) + 1)]
     for spot in trail:
-        rope_map[((max_y) - spot[1])][(spot[0] - min_x)] = "<>"
-    for idx, rope in enumerate(ropes):
-        rope_map[((max_y) - rope[1])][(rope[0] - min_x)] = str(idx).zfill(2)
+        rope_map[((max_y) - spot[1])][(spot[0] - min_x)] = "🔷"
+    for idx, rope in enumerate(ropes[::-1]):
+        if idx == 9:
+            rope_map[((max_y) - rope[1])][(rope[0] - min_x)] = "🟡"
+        elif idx == 0:
+            rope_map[((max_y) - rope[1])][(rope[0] - min_x)] = "🔴"
+        else:
+            rope_map[((max_y) - rope[1])][(rope[0] - min_x)] = "⚪"
+            # rope_map[((max_y) - rope[1])][(rope[0] - min_x)] = str(idx).zfill(2)
     for line in rope_map:
-        print(line)
+        print("".join(line))
 
 
-def part_one(input_filename):
+def part_one(input_filename, put_on_a_show=False):
     input = helpers.parse_input(input_filename)
     head_pos = (0, 0)
     tail_pos = (0, 0)
@@ -62,6 +68,10 @@ def part_one(input_filename):
             if tail_rel in rel_map:
                 tail_pos = numpy.add(tail_pos, rel_map[tail_rel])
                 positions.add(tuple(tail_pos))
+            if put_on_a_show:
+                plot_ropes([head_pos, tail_pos], positions)
+                time.sleep(0.05)
+                clear()
     return len(positions)
 
 
@@ -82,7 +92,7 @@ def part_two(input_filename, put_on_a_show=False):
                     positions.add(tuple(knot_pos[idx_mod]))
             if put_on_a_show:
                 plot_ropes(knot_pos, positions)
-                time.sleep(0.1)
+                time.sleep(0.05)
                 clear()
     return len(positions)
 
@@ -90,8 +100,10 @@ def part_two(input_filename, put_on_a_show=False):
 
 if __name__ == "__main__":
     print("*** PART ONE ***\n")
-    print(f"Test result = {part_one('inputtest.txt')}\n")
-    print(f"REAL RESULT = {part_one('input.txt')}\n\n")
+    # print(f"Test result = {part_one('inputtest.txt')}\n")
+    # print(f"REAL RESULT = {part_one('input.txt')}\n\n")
+    print(f"REAL RESULT = {part_one('waginput.txt', put_on_a_show=True)}\n\n")
     print("*** PART TWO ***\n")
-    print(f"Test result = {part_two('inputtest.txt')}\n")
-    print(f"REAL RESULT = {part_two('input.txt', put_on_a_show=False)}")
+    # print(f"Test result = {part_two('inputtest.txt')}\n")
+    # print(f"REAL RESULT = {part_two('input.txt', put_on_a_show=True)}")
+    # print(f"REAL RESULT = {part_two('waginput.txt', put_on_a_show=True)}")
--- 10/exercise_10.py
+++ 10/exercise_10.py
@@ -1,22 +1,51 @@
 from helpers import helpers
 
 
+IMPORTANT_CYCLES = [20, 60, 100, 140, 180, 220]
+
+
 def part_one(input_filename):
-    input = helpers.parse_input(input_filename)
-    if not input:
-        return "*** NO INPUT SUPPLIED ***"
-    # do stuff here
-    output = input
-    return output
+    input_text = helpers.parse_input(input_filename)
+    cycle, register_x, score = 1, 1, 0
+    for line in input_text:
+        if cycle in IMPORTANT_CYCLES:
+            score += register_x * cycle
+        cycle += 1
+        if line == "noop":
+            continue
+        _, mag = line.split(" ")
+        if cycle in IMPORTANT_CYCLES:
+            score += register_x * cycle
+        cycle += 1
+        register_x += int(mag)
+    return score
+
+
+def write_pixel(drawing, cycle, pos):
+    symbol = "#" if (cycle % 40) - 1 <= pos <= (cycle % 40) + 1 else "."
+    for row in drawing:
+        if len(row) >= 40:
+            continue
+        else:
+            row.append(symbol)
+            break
 
 
 def part_two(input_filename):
-    input = helpers.parse_input(input_filename)
-    if not input:
-        return "*** NO INPUT SUPPLIED ***"
-    # do stuff here
-    output = input
-    return output
+    input_text = helpers.parse_input(input_filename)
+    cycle, register_x = 0, 1
+    drawing = [[] for _ in range(6)]
+    for line in input_text:
+        write_pixel(drawing, cycle, register_x)
+        cycle += 1
+        if line == "noop":
+            continue
+        write_pixel(drawing, cycle, register_x)
+        cycle += 1
+        _, mag = line.split(" ")
+        register_x += int(mag)
+    for row in drawing:
+        print(row)
 
 
 if __name__ == "__main__":
