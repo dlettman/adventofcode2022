1f6fa819fc7b69105666164529e2222b0f74989c
dlettman <derek.lettman@gmail.com>
2022-12-17 14:11:05 -0800

    cleanup day 17

--- 17/exercise_17.py
+++ 17/exercise_17.py
@@ -31,46 +31,49 @@ def take_snapshot_of_top_rows(tetris_board, max_y):  # took a lot of tuning to f
     return tuple(snapshot)
 
 
+def rocks_fall_everyone_dies(tetris_board, highest_y_point, shape_cycle, gas_cycle):
+    next_rock = next(shape_cycle)
+    origin = (2, highest_y_point + 3)
+    still_falling = True
+    while still_falling:
+        gas_symbol = next(gas_cycle)
+        gas_dir = GAS_DIR_MAP[gas_symbol]  # gas blows
+        for coord in next_rock:
+            can_move = True
+            try:
+                delta_coord = tuple(numpy.add(coord, origin))
+                if (tuple(numpy.add(delta_coord, gas_dir)) in tetris_board) or (
+                        numpy.add(delta_coord, gas_dir)[0] < 0) or (tuple(numpy.add(delta_coord, gas_dir))[1] < 0) or (numpy.add(delta_coord, gas_dir)[0] > 6):
+                    can_move = False
+                    break
+            except IndexError:
+                can_move = False
+        if can_move:
+            origin = tuple(numpy.add(origin, gas_dir))
+        for coord in next_rock:
+            delta_coord = numpy.add(coord, origin)
+            if tuple(numpy.add(delta_coord, (0, -1))) in tetris_board or tuple(numpy.add(delta_coord, (0, -1)))[
+                0] < 0 or tuple(numpy.add(delta_coord, (0, -1)))[1] < 0:
+                still_falling = False
+                break
+        if not still_falling:
+            for coord in next_rock:
+                tetris_board.add(tuple(numpy.add(coord, origin)))
+                highest_y_point = (max([coord[1] for coord in tetris_board]) + 1)
+        else:
+            origin = numpy.add(origin, (0, -1))
+    return highest_y_point
+
+
 def part_one(input_filename):
-    shape_cycle = deepcopy(SHAPE_CYCLE)
-    height_map = {}
     input = helpers.parse_input(input_filename)
-    gas_pattern = cycle([char for char in input][0])
+    shape_cycle = deepcopy(SHAPE_CYCLE)
+    gas_cycle = cycle([char for char in input][0])
     highest_y_point = 0
     tetris_board = set()
     for _ in range(2022):
-        next_rock = next(shape_cycle)
-        origin = (2, highest_y_point + 3)
-        still_falling = True
-        while still_falling:
-            gas_symbol = next(gas_pattern)
-            gas_dir = GAS_DIR_MAP[gas_symbol] # gas blows
-            for coord in next_rock:
-                can_move = True
-                try:
-                    delta_coord = tuple(numpy.add(coord, origin))
-                    if (tuple(numpy.add(delta_coord, gas_dir)) in tetris_board) or (numpy.add(delta_coord, gas_dir)[0] < 0) or (tuple(numpy.add(delta_coord, gas_dir))[1] < 0):
-                        can_move = False
-                        break
-                    elif (numpy.add(delta_coord, gas_dir)[0] > 6):
-                        can_move = False
-                        break
-                except IndexError:
-                    can_move = False
-            if can_move:
-                origin = tuple(numpy.add(origin, gas_dir))
-            for coord in next_rock:
-                delta_coord = numpy.add(coord, origin)
-                if tuple(numpy.add(delta_coord, (0, -1))) in tetris_board or tuple(numpy.add(delta_coord, (0, -1)))[0] < 0 or tuple(numpy.add(delta_coord, (0, -1)))[1] < 0:
-                    still_falling = False
-                    break
-            if not still_falling:
-                for coord in next_rock:
-                    tetris_board.add(tuple(numpy.add(coord, origin)))
-                    highest_y_point = max((max([coord[1] for coord in tetris_board]) + 1), highest_y_point)
-                    height_map[_] = highest_y_point
-            else:
-                origin = numpy.add(origin, (0, -1))
+        new_peak = rocks_fall_everyone_dies(tetris_board, highest_y_point, shape_cycle, gas_cycle)
+        highest_y_point = max(new_peak, highest_y_point)
     return highest_y_point
 
 
@@ -79,63 +82,24 @@ def part_two(input_filename):
     height_map = {}
     target_round = 1000000000000
     input = helpers.parse_input(input_filename)
-    gas_pattern = cycle([char for char in input][0])
+    gas_cycle = cycle([char for char in input][0])
     highest_y_point = 0
     tetris_board = set()
     snapshots = {}
-    print(len(input[0]))
-    for round in range(1000000):
-        next_rock = next(shape_cycle)
-        origin = (2, highest_y_point + 3)
-        still_falling = True
-        while still_falling:
-            gas_symbol = next(gas_pattern)
-            gas_dir = GAS_DIR_MAP[gas_symbol] # gas blows
-            for coord in next_rock:
-                can_move = True
-                try:
-                    delta_coord = tuple(numpy.add(coord, origin))
-                    if (tuple(numpy.add(delta_coord, gas_dir)) in tetris_board) or (numpy.add(delta_coord, gas_dir)[0] < 0) or (tuple(numpy.add(delta_coord, gas_dir))[1] < 0):
-                        can_move = False
-                        break
-                    elif (numpy.add(delta_coord, gas_dir)[0] > 6):
-                        can_move = False
-                        break
-                except IndexError:
-                    can_move = False
-            if can_move:
-                origin = tuple(numpy.add(origin, gas_dir))
-            for coord in next_rock:
-                delta_coord = numpy.add(coord, origin)
-                if tuple(numpy.add(delta_coord, (0, -1))) in tetris_board or tuple(numpy.add(delta_coord, (0, -1)))[0] < 0 or tuple(numpy.add(delta_coord, (0, -1)))[1] < 0:
-                    still_falling = False
-                    break
-            if not still_falling:
-                for coord in next_rock:
-                    tetris_board.add(tuple(numpy.add(coord, origin)))
-                highest_y_point = max((max([coord[1] for coord in tetris_board]) + 1), highest_y_point)
-                snapshot = take_snapshot_of_top_rows(tetris_board, highest_y_point)
-                height_map[round] = highest_y_point
-                if snapshot in snapshots:
-                    snapshots[snapshot].append(round)
-                    round_entering_cycle = snapshots[snapshot][0]
-                    round_exiting_cycle = round
-                    height_entering_cycle = height_map[snapshots[snapshot][0]]
-                    height_per_cycle = height_map[round] - height_entering_cycle
-                    cycle_length = round_exiting_cycle - round_entering_cycle
-                    number_of_reps, leftover = divmod(target_round - round_entering_cycle, cycle_length)
-                    return height_entering_cycle + (number_of_reps * height_per_cycle) + height_map[round_entering_cycle + leftover] - (height_map[round_entering_cycle]) - 1
-                else:
-                    snapshots[snapshot] = [round]
-
-            else:
-                origin = numpy.add(origin, (0, -1))
-
-    print(snapshots)
-    for key, value in snapshots.items():
-        if len(value) > 1:
-            print(value, "\n")
-    return highest_y_point
+    for round in range(1000000):  # arbitrary "big enough" number
+        new_peak = rocks_fall_everyone_dies(tetris_board, highest_y_point, shape_cycle, gas_cycle)
+        highest_y_point = max(new_peak, highest_y_point)
+        snapshot = take_snapshot_of_top_rows(tetris_board, highest_y_point)
+        height_map[round] = highest_y_point
+        if snapshot in snapshots:
+            round_entering_cycle = snapshots[snapshot][0]
+            height_entering_cycle = height_map[snapshots[snapshot][0]]
+            height_per_cycle = height_map[round] - height_entering_cycle
+            cycle_length = round - round_entering_cycle
+            number_of_reps, leftover = divmod(target_round - round_entering_cycle, cycle_length)
+            return height_entering_cycle + (number_of_reps * height_per_cycle) + height_map[round_entering_cycle + leftover] - (height_map[round_entering_cycle]) - 1
+        else:
+            snapshots[snapshot] = [round]
 
 
 if __name__ == "__main__":
