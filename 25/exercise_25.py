b0cb4214cf4ce662cd3249f23e768ea2ed516c26
dlettman <derek.lettman@gmail.com>
2022-12-24 23:09:45 -0800

    solve day 25

--- 25/exercise_25.py
+++ 25/exercise_25.py
@@ -1,21 +1,43 @@
 from helpers import helpers
 
+
 def part_one(input_filename):
     input = helpers.parse_input(input_filename)
-    if not input:
-        return "*** NO INPUT SUPPLIED ***"
-    # do stuff here
-    output = input
-    return output
+    score = 0
+    for line in input:
+        thisnum = 0
+        for idx, char in enumerate(line[::-1]):
+            power = 5 ** idx
+            if char in ["0", "1", "2"]:
+                thisnum += power * int(char)
+            elif char == "-":
+                thisnum -= power * 1
+            elif char == "=":
+                thisnum -= power * 2
+        score += thisnum
+    return count(score)
 
 
-def part_two(input_filename):
-    input = helpers.parse_input(input_filename)
-    if not input:
-        return "*** NO INPUT SUPPLIED ***"
-    # do stuff here
-    output = input
-    return output
+def count(score):
+    string = ""
+    while score != 0:
+        remaining = score % 5
+        if remaining == 0:
+            string = "0" + string
+        elif remaining == 1:
+            string = "1" + string
+            score -= 1
+        elif remaining == 2:
+            string = "2" + string
+            score -= 2
+        elif remaining == 3:
+            string = "=" + string
+            score += 2
+        elif remaining == 4:
+            string = "-" + string
+            score += 1
+        score = score // 5
+    return string
 
 
 if __name__ == "__main__":
@@ -23,5 +45,4 @@ if __name__ == "__main__":
     print(f"Test result = {part_one('inputtest.txt')}\n")
     print(f"REAL RESULT = {part_one('input.txt')}\n\n")
     print("*** PART TWO ***\n")
-    print(f"Test result = {part_two('inputtest.txt')}\n")
-    print(f"REAL RESULT = {part_two('input.txt')}")
+    print("THERE AIN'T NO PART TWO, COWBOY")
